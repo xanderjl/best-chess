a1827f3d16bed9aaa1eff3635c1e06c67b0f524a
Xander Low <xander@xandydandy.com>
2023-08-04 22:55:53 -0700

    added: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import solidjs from '@astrojs/solid-js';
+import vercel from '@astrojs/vercel'
 import pandacss from '@pandacss/dev/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss(), solidjs()]
+  integrations: [pandacss(), solidjs(), vercel()]
 });
