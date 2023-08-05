4f4539a7f9ca575a6e4aadd30d74299fe78a286c
Xander Low <xander@xandydandy.com>
2023-08-04 22:57:24 -0700

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import solidjs from '@astrojs/solid-js';
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 import pandacss from '@pandacss/dev/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss(), solidjs(), vercel()]
+  integrations: [pandacss(), solidjs()],
+  adapter: vercel(),
+  output: 'server'
 });
