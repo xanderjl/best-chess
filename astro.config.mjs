c893cef31ed8ec244301c82c2fa105956035018a
Xander Low <xander@xandydandy.com>
2023-08-16 16:29:16 -0700

    lint fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import solidjs from '@astrojs/solid-js';
 import vercel from '@astrojs/vercel/serverless';
 import pandacss from '@pandacss/astro';
 import { defineConfig } from 'astro/config';
-import auth from "auth-astro";
+import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
